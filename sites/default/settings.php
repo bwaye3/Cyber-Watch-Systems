e1930496cb590d9d4a87173b728e4ca04860011a
Brad Waye <bradwaye@gmail.com>
2020-02-24 14:21:31 -0500

    settings file

--- sites/default/settings.php
+++ sites/default/settings.php
@@ -766,3 +766,18 @@ $databases['default']['default'] = array(
   'driver' => 'mysql',
 );
 $settings['config_sync_directory'] = 'sites/default/files/config_MFmKIXnLPgIC7yDATMT-NCAJ7ugTQEAIoSL3zPSneBuoDyjWFENG9kMFAhq72UzI87EzGrEUzg/sync';
+
+/**
+ * Load local development override configuration, if available.
+ *
+ * Use settings.local.php to override variables on secondary (staging,
+ * development, etc) installations of this site. Typically used to disable
+ * caching, JavaScript/CSS compression, re-routing of outgoing emails, and
+ * other things that should not happen on development and testing sites.
+ *
+ * Keep this code block at the end of this file to take full effect.
+ */
+#
+if (file_exists($app_root . '/' . $site_path . '/settings.local.php')) {
+   include $app_root . '/' . $site_path . '/settings.local.php';
+ }
